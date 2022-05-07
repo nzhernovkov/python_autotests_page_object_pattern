41c4c6216dafdae67c3b733e65a74a58c399f54a
Zhernovkov Nikita <zhernovkov@proitr.ru>
2022-05-07 19:45:02 +0500

    remove unused import

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,8 @@
-import time
-
 from .pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
